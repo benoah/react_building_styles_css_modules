d44c06df061eb84eba4f2a56d8adeda4ca93fb46
ben <noahbenaas@gmail.com>
2021-07-14 13:54:47 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import Grid from './Grid/Grid';
 import Rocket from './Rocket/Rocket';
 import Smoke from './Rocket/Smoke';
 
+import facts from './NASA_facts.json';
+
 //styles
 import styles from './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      facts,
+    };
+  }
+  updateClicked(id) {
+    let stateCopy = Object.assign({}, this.state);
+    stateCopy.facts[id].clicked = true;
+    this.setState(stateCopy);
+  }
   render() {
     return (
       <div className={styles.app}>
         <Title />
         <Background />
-        <Grid />
+        <Grid facts={this.state.facts} updateClicked={this.updateClicked.bind(this)}/>
         <Rocket />
         <Smoke />
       </div>
